bd0f2fb37762b824579a95738971b216d237f216
Djon <bugrimdjon@gmail.com>
2025-05-13 23:50:29 +0300

    Начал реализовывать интерфейс "ITradeAnalysisService" - логика обработки всех сделок

---
+++ Interfaces/ITradeAnalysisService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot_analysis.Interfaces
+{
+    internal interface ITradeAnalysisService
+    {
+
+        Task AnalyzeTradesAsync(); //анализ ручных сделок 
+    }
+
+}
